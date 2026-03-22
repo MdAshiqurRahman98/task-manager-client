a93b890794b6d6a592c58ee943ac5e348414ae22
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2026-03-22 17:55:41 +0600

    App.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import { Toaster } from "react-hot-toast"
 import MainLayout from "./components/layouts/MainLayout"
 
 function App() {
 
   return (
     <>
-      
+      <Toaster></Toaster>
       <MainLayout></MainLayout>
     </>
   )
